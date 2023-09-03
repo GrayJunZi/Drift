1ae96e4502b8566230273dae54963e8c59e967d3
GrayJunZi <GrayJunZi@outlook.com>
2023-09-03 23:48:04 +0800

    029. 使用Link组件添加路由

--- discover-coffee-stores/pages/coffee-store/[id].js
+++ discover-coffee-stores/pages/coffee-store/[id].js
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
     const router = useRouter();
-    console.log('router', router);
-    return <div>Coffee Store Page {router.query.id}</div>;
+    return <div>
+        Coffee Store Page {router.query.id}
+        <Link href='/'>Back to home</Link>
+    </div>;
 };
 
 export default CoffeeStore;
